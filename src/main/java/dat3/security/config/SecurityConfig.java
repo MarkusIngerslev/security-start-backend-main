53228bf03fc34b0112d7505be3618ca8d5fe396f
MarkusIngerslev <markusolsen2004@gmail.com>
2024-03-04 16:55:37 +0100

    Start, and test security features in the backend

--- src/main/java/dat3/security/config/SecurityConfig.java
+++ src/main/java/dat3/security/config/SecurityConfig.java
@@ -71,6 +71,7 @@ public class SecurityConfig {
             .requestMatchers(mvcMatcherBuilder.pattern("/error")).permitAll()
 
             //This is for demo purposes only, and should be removed for a real system
+            //.requestMatchers(mvcMatcherBuilder.pattern(HttpMethod.GET, "/api/demo/admin")).hasAuthority("ADMIN")
             //.requestMatchers(mvcMatcherBuilder.pattern(HttpMethod.GET, "/api/test/user-only")).hasAuthority("USER")
             //.requestMatchers(mvcMatcherBuilder.pattern(HttpMethod.GET, "/api/test/admin-only")).hasAuthority("ADMIN")
 
